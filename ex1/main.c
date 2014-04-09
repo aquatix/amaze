15d6c9fbc8c19ea77291e963373df00fd4ee37bd
Michiel Scholten <michiel@aquariusoft.org>
2014-04-09 10:37:11 +0200

    20031008_ex1.tar.bz2

--- ex1/main.c
+++ ex1/main.c
@@ -4,14 +4,11 @@
 /*
 #include <util/glutil.h>
 */
-/*
-include </home/mbscholt/mydocs/vu/20032004/cg/graphics/util/glutil.h>
-*/
 
 /*
  * Computer Graphics - Exercise 1
  *
- * Version 2003-10-10
+ * Version 2003-10-08
  * Michiel Scholten [ mbscholt@cs.vu.nl ]
  */
 
@@ -20,7 +17,7 @@ void display(void)
 {
 /*	glClearColor(1.0, 0.2, 1.0, 1.0); roze */
 	/* Get a nice darkblue as background */
-	glClearColor(0.2, 0.8, 1.0, 1.0);
+	glClearColor(0.0, 0.0, 0.5, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 
 }
@@ -57,8 +54,31 @@ void mouse(int btn, int btn_state, int x, int y)
 	}
 }
 
+void handle_menu(int whichone)
+{
+	switch (whichone)
+	{
+		case 1: /* item 1 */
+			printf("menu :: entry 1\n");
+			break;
+		case 2: /* item 2 */
+			printf("menu :: entry 2\n");
+			break;
+		case 3: /* item 3 */
+			printf("menu :: entry 3\n");
+			break;
+		case 4:
+			printf("menu :: quit entry chosen, exiting\n");
+			exit(0);
+			break;
+	}
+	
+}
+
 int main(int argc, char **argv)
 {
+	int sub_menu, top_menu;
+
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB);
 	glutInitWindowSize(800,600);
@@ -70,8 +90,13 @@ int main(int argc, char **argv)
 	glutKeyboardFunc(keyboard);
 	glutMouseFunc(mouse);
 
-	sub_menu = glutCreateMenu(dummy_menu);
-	glutAddMenuEntry(
+	sub_menu = glutCreateMenu(handle_menu);
+	glutAddMenuEntry("entry 1", 1);
+	glutAddMenuEntry("entry 2", 2);
+	glutAddMenuEntry("entry 3", 3);
+	/*glutCreateMenu(top_menu);*/
+	glutAddMenuEntry("Quit [q, ESC]", 4);
+	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
 	glutMainLoop();
 }
