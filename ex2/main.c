5cbc894d6b915b936e6e42eebaf5798a5be1a9d5
Michiel Scholten <michiel@aquariusoft.org>
2014-04-09 10:37:12 +0200

    20031121c_ex2_final.tar.bz2

--- ex2/main.c
+++ ex2/main.c
@@ -1,28 +1,36 @@
 #include <GL/glut.h>
 #include <stdio.h>
 
+#if 0
 #include </home/mbscholt/mydocs/vu/20032004/cg/exercises/util/glutil.h>
-/*
 #include </home/mbscholt/xc_home/mydocs/vu/20032004/cg/exercises/util/glutil.h>
 #include "util/glutil.h"
-*/
+#endif
 
 /*
  * Computer Graphics - Exercise 2
  * Solar System
  *
  * Version 2003-11-21
- * Michiel Scholten [ mbscholt@cs.vu.nl ]
+ * Michiel Scholten [ mbscholt@cs.vu.nl | 1204467 ]
  */
 
-#define F16_MODEL 0
-#define DEBUG
+#define F16_MODEL 1
+#define NODEBUG
+#define INFO
 
 #ifdef DEBUG
 #define dprint printf
 #endif
 #ifndef DEBUG
 #define dprint (void)
+#endif
+
+#ifdef INFO
+#define iprint printf
+#endif
+#ifndef INFO
+#define iprint (void)
 #endif
 
 double time;
@@ -39,11 +47,12 @@ point3 pyramid[5] = {{-1.0, -1.0, 1.0}, {1.0, -1.0, 1.0}, {1.0,-1.0, -1.0}, {-1.
 point3 cube[8] = {{-1.0, -1.0, -1.0}, {1.0, -1.0, -1.0},
 	{1.0, 1.0, -1.0}, {-1.0,1.0,-1.0}, {-1.0, -1.0, 1.0},
 	{1.0, -1.0, 1.0}, {1.0, 1.0, 1.0}, {-1.0, 1.0, 1.0}};
+
 point3 colors[6] = {{175.0, 0.0, 0.0}, {0.0, 175.0, 0.0}, {0.0, 0.0, 175.0}, {50.0, 50.0, 0.0}, {0.0, 50.0, 50.0}, {50.0, 0.0, 50.0}};
 
 void idle(void)
 {
-	/* Get the cpu loaded for 100% while updating continously :) */
+	/* Get the cpu loaded for 100% while updating continously :) | -> Not used in this exercise */
 //	glutPostRedisplay();
 }
 
@@ -53,7 +62,10 @@ void rotatePyramid(int value)
 {
 	if (mousedown && animate)
 	{
-		/* Every 30msec, but only if the left mouse button is pushed. Rotating ones every 3sec */
+		/*
+		 * Every 30msec, but only if the left mouse button is pushed and animation is enabled by user.
+		 * Rotating once every 3sec
+		 */
 		theta_pyramid += 3.6;
 		if ( theta_pyramid >= 360.0 )
 		{
@@ -68,7 +80,7 @@ void rotateCube(int value)
 {
 	if (animate)
 	{
-		/* Every 20msec, but only if user has enabled it with the spacebar */
+		/* Every 20msec, but only if user has enabled the animation with the spacebar */
 		theta_cube += 2;
 		if ( theta_cube >= 360.0 )
 		{
@@ -83,7 +95,7 @@ void rotateF16(int value)
 {
 	if (animate)
 	{
-		/* Every 10msec, but only if user has enabled it with the spacebar */
+		/* Every 10msec, but only if user has enabled the animation with the spacebar */
 		theta_f16 += 4;
 		if ( theta_f16 >= 360.0 )
 		{
@@ -95,39 +107,11 @@ void rotateF16(int value)
 }
 ////////////////////////////////////// Rotating functions <
 
-////////////////////////////////////// Ish >
-int copyPoint3v(point3 *src, point3 *dest, int number)
-{
-	int i;
-	for (i=0; i<number; i++)
-	{
-		copyPoint3(*dest[i], *src[i]);
-	}
-	return 1;
-}
-
-int copyPoint3(point3 *src, point3 *dest)
-{
-/*
-	int i;
-	for (i = 0; i<4; i++)
-	{
-		*dest[i] = *src[i];
-	}
-*/
-	dest = src;
-printf("point: %i\n", dest);
-	return 1;
-}
-////////////////////////////////////// Ish <
-
 ////////////////////////////////////// Drawing >
 void drawPyramid()
 {
-	point3 base[4];
-	point3 triangle[3];
-	/* we want a pyramid with a square base, every side a different colour */
-	//glColor3ub(0.0, 175.0, 0.0);
+	/* We want a pyramid with a square base, every side a different colour */
+
 	/* Draw the square base */
 	glBegin(GL_POLYGON);
 		glColor3fv(colors[0]);
@@ -165,23 +149,12 @@ void drawPyramid()
 		glVertex3fv(pyramid[0]);
 		glVertex3fv(pyramid[4]);
 	glEnd();
+
 	/* Done :) */
 }
 
 int drawCube()
 {
-#if 0
-	point3 vertices[4];
-	point3 color;
-
-	copyPoint3(&cube[0], &vertices[0]);
-	copyPoint3(&cube[1], &vertices[3]);
-	copyPoint3(&cube[2], &vertices[2]);
-	copyPoint3(&cube[3], &vertices[1]);
-	copyPoint3(&colors[0], &color); 
-//	drawSquare(&cube, &color);
-#endif
-
 	glBegin(GL_POLYGON);
 		glColor3fv(colors[0]);
 		glVertex3fv(cube[4]);
@@ -229,8 +202,8 @@ int drawCube()
 		glVertex3fv(cube[3]);
 		glVertex3fv(cube[7]);
 	glEnd();
-
 }
+
 #if 0
 /*
 int drawSquare(point3 *vertices, point3 colors)
@@ -262,7 +235,7 @@ printf("vertex 3: %i\n", vertices[3]);
 		glVertex3fv(vertices[3]);
 	glEnd();
 }
-#endif
+
 int drawTriangle(point3 *vertices, point3 colors)
 {
 	/* set the color of the square */
@@ -283,7 +256,7 @@ int drawTriangle(point3 *vertices, point3 colors)
 		glVertex3fv(vertices[2]);
 	glEnd();
 }
-////////////////////////////////////// Drawing <
+#endif
 
 void display(void)
 {
@@ -292,94 +265,94 @@ void display(void)
 
 	glLoadIdentity();
 	gluLookAt(0.0, 0.0, 20.0,	// <- eye
-			0.0, 0.0, 0.0,	// <- center
-			0.0, 1.0, 0.0);	// <- up
+		0.0, 0.0, 0.0,		// <- center
+		0.0, 1.0, 0.0);		// <- up
 	
-	//drawSquare();
-
+	/* Pyramid > */
 	glPushMatrix();
-#if 0
-	glTranslatef(0.0f, 0.0f, 3.0f);
-#endif
-		glRotatef(theta_pyramid, 0, 1, 0);
+		glRotatef(theta_pyramid, 1, 0, 0);
 		glScaled(2.0,2.0,2.0);
 		drawPyramid();
-
 	glPopMatrix();
+	/* Pyramid < */
 
-	glRotatef(theta_cube, 0.5, 1, 0);
 	glPushMatrix();
+	glRotatef(theta_cube, 0.5, 1, 0);
 		glTranslatef(0.0f, 0.0f, 10.0f);
+		/* Cube > */
 		glPushMatrix();
-		//glScaled(4.0,4.0,4.0);
-		//glRotatef(theta_cube, 0.5, 1, 0);	/* Some nice rotation */
-		glRotatef(theta_cube, 1, 0, 0);
-		drawCube();
+			//glRotatef(theta_cube, 0.5, 1, 0);	/* Some nice rotation */
+			glRotatef(theta_cube, 1, 0, 0);		/* Rotation around x-axis [as stated on website */
+			drawCube();
 		glPopMatrix();
+		/* Cube < */
 
-		
+		/* F16 > */
 		glPushMatrix();
 			//glTranslatef(1.0f, 0.0f, 3.0f);
-		glRotatef(theta_f16, 0.0, 1, 0);
-			glTranslatef(0.0f, 0.0f, 3.0f);
+			glRotatef(-theta_f16, 0.0, 1.0, 0.0);
+			glTranslatef(0.0f, 0.0f, 5.0f);
 			//glRotatef(theta_f16, 0.0, 1.5, 0);
-			drawCube();
-			//drawF16();
-			//glCallList(F16_MODEL);
+			glRotatef(-90, 0.0, 1.0, 0.0);
+			glScaled(2.0,2.0,2.0);
+			//drawCube();
+			glCallList(F16_MODEL);
 		glPopMatrix();
+		/* F16 < */
 	glPopMatrix();
-	
 
-
-	
 	glFlush();
 	glutSwapBuffers();
 }
 
-////////////////////////////////////// Loading stuff >
-#if 0
-int loadModel(char *filename[], int nrOfFiles, int identifier)
+void reshape_now(GLsizei w, GLsizei h)
 {
-	/* Load a model from file, in our case the F16 plane
-	 * Check for existence of file, return 0 if error, otherwise 1 [or the number of vertices or something?]
-	 * If succesfull, load everything in a list
-	 */
-	int i;
-	FILE *in;
-	for (i = 0; i < nrOfFiles; i++)
+	double whRatio = (double)h / (double)w;
+	glViewport(0, 0, w, h);
+	dprint("%f\n", whRatio);
+	fflush(stdout);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(-0.3, 0.3, -whRatio * 0.3 ,whRatio * 0.3 ,0.3 ,100);
+	glMatrixMode(GL_MODELVIEW);
+#if 0
+	/* adjust clipping box *//*
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, w/h, 2.0, 40.0);
+	glMatrixMode(GL_MODELVIEW);*/
+	/* adjust viewport and draw the picture */
+	if (h < w)
 	{
-		printf("%i\n", i);
-		if ((in = fopen(filename[i], "r")) == NULL)
-		{
-			printf("error %i\n", i);
-			printf("Unable to open the file [file #%i, name: %s]\n", i, filename[i]);
-			return 0;
-		}
-		printf("good %i\n", i);
-		printf("%s\n", *filename[i]);
+		glViewport( w / 2 - h / 2, 0, h, h);
+	} else
+	{
+		glViewport( 0, h / 2 - w / 2, w, w);
 	}
-	//scanf();
-}
 #endif
+}
+////////////////////////////////////// Drawing <
+
+////////////////////////////////////// Loading stuff >
 int loadModel(char *filename)
 {
 	int vertex, nrVertices;
 	char identifier[20]; // string of maxlen 20
 	GLfloat vertex_x, vertex_y, vertex_z, normal_x, normal_y, normal_z;
-	int thisType, status;
+	int status;
 	
 	FILE *in;
-	printf("Loading \"%s\" ... \n", filename);
+	iprint("Loading \"%s\" ... \n", filename);
 	if ((in = fopen(filename, "r")) == NULL)
 	{
-		printf("Unable to open the file [file: %s]\n", *filename);
+		iprint("Unable to open the file [file: %s]\n", *filename);
 		return 0;
 	}
 	/* Load stuff */
 	while (!feof(in))
 	{
 		status = fscanf(in, " %s %i \n", &identifier, &nrVertices);
-dprint(" - read type %s, nr of vertices is %i\n", identifier, nrVertices);
+		dprint(" - read type %s, nr of vertices is %i\n", identifier, nrVertices);
 
 		if (status == EOF)
 		{
@@ -387,13 +360,13 @@ dprint(" - read type %s, nr of vertices is %i\n", identifier, nrVertices);
 		}
 		if (status < 2)
 		{
-			dprint("ERROR @ loadModel :: Reading of identifier and number failed!\n");
+			iprint("ERROR @ loadModel :: Reading of identifier and number failed!\n");
 			return 0;
 		}
 
 		/* 
-		 * Look which kind of figure we have to draw
-		 * Chose from POINTS, LINES, LINE_STRIP, LINE_LOOP, TRIANGLES, QUADS, TRIANGLE_STRIP, TRIANGLE_FAN, QUAD_STRIP or POLYGON
+		 * Look which kind of figure we have to draw and start the figure
+		 * Choose from POINTS, LINES, LINE_STRIP, LINE_LOOP, TRIANGLES, QUADS, TRIANGLE_STRIP, TRIANGLE_FAN, QUAD_STRIP or POLYGON
 		 */
 		if (strcmp(identifier, "POINTS") == 0)
 		{
@@ -428,20 +401,20 @@ dprint(" - read type %s, nr of vertices is %i\n", identifier, nrVertices);
 		} else
 		{
 			/* Return an error, or at least a -1 */
-			printf("ERROR @ loadModel :: unknown identifier!\n");
+			iprint("ERROR @ loadModel :: unknown identifier!\n");
 			return -1;
 		}
 		/* Load vertices and draw the thing */
-		glColor3fv(colors[0]);
+		//glColor3fv(colors[0]);
 		for (vertex = 0; vertex < nrVertices; vertex++)
 		{
 			fscanf(in, " %f %f %f %f %f %f\n", &vertex_x, &vertex_y, &vertex_z, &normal_x, &normal_y, &normal_z);
-			//glNormal3f(normal_x, normal_y, normal_z);
+			glNormal3f(normal_x, normal_y, normal_z);
 			glVertex3f(vertex_x, vertex_y, vertex_z);
 		}
-		glEnd();
+		glEnd();	/* Done with this figure */
 	}
-	printf("done\n");
+	iprint("done\n");
 	return 1;
 }
 ////////////////////////////////////// Loading stuff <
@@ -512,62 +485,29 @@ void handle_menu(int whichone)
 }
 ////////////////////////////////////// Handlers <
 
-void reshape_now(GLsizei w, GLsizei h)
-{
-	double whRatio = (double)h / (double)w;
-	glViewport(0, 0, w, h);
-	printf("%f\n", whRatio);
-	fflush(stdout);
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	glFrustum(-0.3, 0.3, -whRatio * 0.3 ,whRatio * 0.3 ,0.3 ,100);
-	glMatrixMode(GL_MODELVIEW);
-#if 0
-	/* adjust clipping box *//*
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluPerspective(45.0, w/h, 2.0, 40.0);
-	glMatrixMode(GL_MODELVIEW);*/
-	/* adjust viewport and draw the picture */
-	if (h < w)
-	{
-		glViewport( w / 2 - h / 2, 0, h, h);
-	} else
-	{
-		glViewport( 0, h / 2 - w / 2, w, w);
-	}
-#endif
-}
-
 int main(int argc, char **argv)
 {
-#if 0
-	char[8][15] f16_files = {"afterburner.sgf", "body.sgf", "bomb.sgf", "cockpit.sgf",
-		"rockets.sgf", "tailfin.sgf", "tailwings.sgf", "wings.sgf"};
-	int i;
-#endif
 	char f16_files[8][40]= {"../models/f-16/afterburner.sgf", "../models/f-16/body.sgf", "../models/f-16/bomb.sgf", "../models/f-16/cockpit.sgf",
 		"../models/f-16/rockets.sgf", "../models/f-16/tailfin.sgf", "../models/f-16/tailwings.sgf", "../models/f-16/wings.sgf"};
 	int i;
 			
-	/* the menus */
-
 	/* initialize glut and the window */
 	glutInit(&argc, argv);
 	/* Use rgb, double buffering and hidden surface removal */
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-//	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 	glutInitWindowSize(800,600);
 	glutInitWindowPosition(0,0);
 	glutCreateWindow("Exercise 2 [Solar system | mbscholt@cs.vu.nl | 1204467]");
 
-	glClearColor(0.0, 0.0, 0.5, 1.0);
+	//glClearColor(0.0, 0.0, 0.5, 1.0);
+	glClearColor(0.0, 0.5, 0.9, 1.0);
 
 	glEnable(GL_DEPTH_TEST);
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 	glEnable(GL_COLOR_MATERIAL);
+	glShadeModel(GL_SMOOTH);
 	
 	/* set various event callback functions */
 	glutReshapeFunc(reshape_now);
@@ -584,33 +524,25 @@ int main(int argc, char **argv)
 	glutAddMenuEntry("quit [q, esc]", 4);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
 
-	//glutTimerFunc(30, rotatePyramid, 0);
-	
-	/* Load the F16 into a list */
-	//void glNewList(GLuint listID, GLenum mode);
+	/* Load the F16 into a display list | void glNewList(GLuint listID, GLenum mode); */
 	glNewList(F16_MODEL, GL_COMPILE);
 	for (i = 0; i < 8; i++)
 	{
-		if (loadModel(&f16_files[i]) == 0)
+		glColor3f(.75, .5, .5);
+		if (strcmp("../models/f-16/cockpit.sgf", f16_files[i]) == 0) glColor3f(.0, .0, .7);
+		if (strcmp("../models/f-16/rockets.sgf", f16_files[i]) == 0) glColor3f(1.0, .0, .0);
+		if (strcmp("../models/f-16/bomb.sgf", f16_files[i]) == 0) glColor3f(.0, 1.0, .0);
+		if (loadModel(f16_files[i]) == 0)
 		{
 			printf("ERROR while loading %s\n", f16_files[i]);
 			exit(1);
 		}
 	}
-#if 0
-		loadModel("../models/f-16/afterburner.sgf");
-		loadModel("../models/f-16/body.sgf");
-		loadModel("../models/f-16/bomb.sgf");
-		loadModel("../models/f-16/cockpit.sgf");
-		loadModel("../models/f-16/rockets.sgf");
-		loadModel("../models/f-16/tailfin.sgf");
-		loadModel("../models/f-16/tailwings.sgf");
-		loadModel("../models/f-16/wings.sgf");
-	//loadModel(&f16_files, 8, F16_MODEL);
-#endif
-	void glEndList(void);
+	glEndList();
 	
 	/* now loop */
 	glutMainLoop();
+
+	return 0;
 }
 
